5866ae1db58fe027d2af10a4ed58ec91170be5cf
Daniel Osborne <Daniel.T.Osborne@usace.army.mil>
2025-03-26 13:32:38 -0700

    Add DoD certs. Container improvements for cloud.

--- Dockerfile
+++ Dockerfile
@@ -3,19 +3,23 @@ ARG BASE_IMAGE=python:3.11-alpine3.19
 FROM $BASE_IMAGE as build
 # ironbank image requires USER root to build
 USER root
-RUN apk add --no-cache zip
+RUN apk add --no-cache zip bash curl openssl
 COPY . /repgen5
 WORKDIR /repgen5
 RUN ./package.sh
+RUN ./add-dod-certs.sh
 
 FROM $BASE_IMAGE
 USER root
 ADD ./requirements.txt /tmp
 # Bug in ironport that doesn't clear the apk cache; see https://repo1.dso.mil/dsop/opensource/python/python-alpine/python-alpine-311/-/issues/35
 RUN rm -rf /var/cache/apk/* ; pip install -r /tmp/requirements.txt ; rm /tmp/requirements.txt
-COPY --from=build /repgen5/build/repgen /
-COPY --from=build /repgen5/converter/convert_report.py /repgen5/converter/convert.sh /converter/
-ADD ./entrypoint.sh /
+
+COPY --from=build /usr/local/share/ca-certificates/* /usr/local/share/ca-certificates/
+RUN /usr/sbin/update-ca-certificates
+COPY --chmod=775 --from=build /repgen5/build/repgen /
+COPY --chmod=775 --from=build /repgen5/converter/convert_report.py /repgen5/converter/convert.sh /converter/
+ADD --chmod=775 ./entrypoint.sh /
 
 # Location of report templates (--in arg)
 VOLUME /input
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+input="-"
+if [ ! -z "${INPUT_FILE}" ]; then
+	scheme="$(echo "${INPUT_FILE}" | cut -d: -f1)"
+	if [ "${scheme}" != "s3" ]; then
+		usage "BATCH_FILE_S3_URL must be for an S3 object; expecting URL starting with s3://"
+	fi
+
+	# mktemp arguments are not very portable.  We make a temporary directory with
+	# portable arguments, then use a consistent filename within.
+	TMPDIR="$(mktemp -d -t tmp.XXXXXXXXX)" || error_exit "Failed to create temp directory."
+	TMPFILE="${TMPDIR}/input-file-temp"
+	OUTPUT="${TMPDIR}/output-file-temp"
+	install -m 0600 /dev/null "${TMPFILE}" || error_exit "Failed to create temp file."
+
+	aws s3 cp "${INPUT_FILE}" - > "${TMPFILE}" || error_exit "Failed to download S3 script."
+	input="${TMPFILE}"
+fi
+
 if [ "$1" = "convert" ]; then
 	shift
 	exec /converter/convert_report.py "$@"
@@ -24,15 +42,21 @@ else
 	[ -n "$COMPATIBILITY_MODE" ] && [ $COMPATIBILITY_MODE -gt 0 ] && args="$args --compatibility"
 	[ -n "$CDA_TIMEOUT" ] && args="$args --timeout $CDA_TIMEOUT"
 	[ -n "$OFFICE_ID" ] && args="$args --office $OFFICE_ID"
-	[ -n "$RUN_DATE" ] && args="$args --date $DATE"
-	[ -n "$RUN_TIME" ] && args="$args --time $TIME"
+	[ -n "$DATE" ] && args="$args --date $DATE"
+	[ -n "$TIME" ] && args="$args --time $TIME"
 	[ -n "$DATA_FILE" ] && args="$args --file \"$DATA_FILE\""
 	# TZ is already handled directly by repgen
 
-	[ $found -eq 0 ] && args="$args -i-"
+	[ $found -eq 0 ] && [ -z $input ] && args="$args -i-"
+	[ ! -z $input ] && args="$args -i$input"
+
+	# If S3 path specified for output, use temp path
+	[ ! -z $OUTPUT_FILE ] && args="$args -i$OUTPUT"
 
 	args="--address $CDA_PRIMARY $args"
 	[ -n "$VERBOSE" ] && [ $VERBOSE -gt 0 ] && set>&2 &&
 		echo /repgen $args "$@">&2
-	exec /repgen $args "$@"
+	/repgen $args "$@"
+
+	[ ! -z $OUTPUT_FILE ] && aws s3 cp ${OUTPUT} ${OUTPUT_FILE}
 fi
